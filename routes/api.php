8383e5a208257e80977d270e6a6591f86941815e
azenko <resseguier@et.esiea.fr>
2017-12-30 12:28:37 +0100

    Adding Menu API URL : /api/menu

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 
+use App\Models\Boisson;
+use App\Models\Sandwitch;
+use App\Models\Dessert;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,3 +27,18 @@ Route::resource('boissons', 'BoissonAPIController');
 Route::resource('desserts', 'DessertAPIController');
 
 Route::resource('sandwitches', 'SandwitchAPIController');
+
+Route::get('/menu', function () {
+
+  $sandwitches = Sandwitch::all()->random(1);
+  $boissons = Boisson::all()->random(1);
+  $desserts = Dessert::all()->random(1);
+
+  $menu = new stdClass();
+  @$menu->generated = true;
+  @$menu->data->boisson = $boissons[0];
+  @$menu->data->sandwitch = $sandwitches[0];
+  @$menu->data->dessert = $desserts[0];
+
+  return json_encode($menu);
+});
